f1de97c59ead0c256b0273dfb0d10dd7cfe0b700
Louis Genestier <louis.genestier@gmail.com>
2022-09-06 15:25:11 +0200

    feat: add ws server

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+import { WebSocketServer } from 'ws'
+
+const PORT = 8080
+
+const start = () => {
+  const wss = new WebSocketServer({
+    port: PORT
+  })
+
+  wss.on('connection', ws => {
+    ws.on('message', data => {
+      console.log(`received data: ${data}`)
+    })
+  })
+}
+
+start()
